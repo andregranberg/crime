dee1c1c89b70a94dced51abc4fbf9c506d2d9b71
André <andre.granberg@live.se>
2021-05-13 19:10:50 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from 'react-bootstrap/Navbar'
+
+import Alert from 'react-bootstrap/Alert'
+
+import Button from 'react-bootstrap/Button'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <header className="App-header">
+
+    <Button variant="danger" size="lg" block>
+    Stäng menyn
+  </Button>
+
+
       </header>
     </div>
   );
